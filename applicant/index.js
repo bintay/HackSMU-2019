38b693a62479ccbd1c2a0bfd8500362b7a698815
Ben Taylor <bentaylor@bentaylor.me>
2019-02-02 11:41:08 -0600

    Basic Information Forms

---
+++ applicant/index.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use('/public', express.static('public'));
+
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname + '/frontend/index.html'));
+});
+
+app.listen(port, function () {
+   console.log(`Listening on port ${port}`);
+});
