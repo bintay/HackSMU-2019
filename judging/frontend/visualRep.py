e347c56ab4194152d13b99398847b42c6ba7c4d7
wdeng112 <46981661+wdeng112@users.noreply.github.com>
2019-02-03 05:24:34 -0600

    Update visualRep.py

--- judging/frontend/visualRep.py
+++ judging/frontend/visualRep.py
@@ -1,33 +1,27 @@
 import numpy as np
 import pandas as pd
 import csv
+from bokeh.themes import built_in_themes
 from bokeh.plotting import figure, output_file, show, curdoc
 from bokeh.layouts import layout,widgetbox
-from bokeh.models import Toggle, BoxAnnotation, CustomJS, Circle, HoverTool, ColumnDataSource, NumeralTickFormatter, OpenURL, TapTool
+from bokeh.models import Toggle, BoxAnnotation, CustomJS, Circle, HoverTool, ColumnDataSource, NumeralTickFormatter, OpenURL, TapTool, Selection
 from bokeh.models.widgets import DataTable, DateFormatter, TableColumn, Panel, Tabs, DateFormatter,TextInput, Button, Toggle
 from datetime import date
 
-
-
-
-
+curdoc().theme = 'dark_minimal'
 output_file("color_scatter.html", title="color_scatter.py example", mode="cdn")
 
-# x = master_sheet['soft_index']
-# y = master_sheet['tech_index']
-# appending values from local csv
+
 df = pd.read_csv("lel.csv")
 sample = df.sample(50)
 source = ColumnDataSource(sample)
 
-# # output to static HTML file (with CDN resources)
 
 TOOLS = "crosshair,pan,wheel_zoom,box_zoom,reset,box_select,lasso_select, tap"
 
-# create a new plot with the tools above, and explicit ranges
 
 
-p = figure(tools=TOOLS, title="Applicant Pool",x_range=(0, 12), y_range=(0, 12))
+p = figure(tools=TOOLS, title="Applicant Pool",x_range=(0, 1.1), y_range=(0, 1.1))
 p.xaxis.axis_label = "Tech Compotency Index"
 p.xaxis.axis_label_text_color = "#aa6666"
 
@@ -40,7 +34,6 @@ hover.tooltips=[
     ('Soft Skill Index', '@soft_index'),
     ('Tech Compotency Index', '@tech_index'),
     ('Code Duration', '@code_time'),
-    ('Code Attempts','@code_attempts'),
     ('Date of Submission','@date'),
     ('Email','@email')
 ]
@@ -59,10 +52,7 @@ p.circle(x = 'tech_index', y = 'soft_index', source = source,  size=20,
                        nonselection_line_alpha=1.0)
 
 
-
-
-
-box = BoxAnnotation(bottom =0, top = 5, left=0, right=5, fill_color='#F01716', fill_alpha=0.1)
+box = BoxAnnotation(bottom =0, top = 0.5, left=0, right=0.5, fill_color='#F01716', fill_alpha=0.1)
 p.add_layout(box)
 
 
@@ -77,36 +67,34 @@ columns = [
         TableColumn(field="email", title="Email"),
         TableColumn(field="date", title="Date of Submission", formatter=DateFormatter()),
         TableColumn(field="code_time", title="Duration of Code"),
-        TableColumn(field="code_attempts", title="Attempts"),
         TableColumn(field="soft_index", title="Soft Skills Index"),
         TableColumn(field="tech_index", title="Tech Skills Index")
     ]
 
 data_table = DataTable(source=source, columns=columns, width=1300, height=1000)
-
-tab2 = Panel(child = data_table, title= "Table")
-
-
 text_input = TextInput(title="Search for Applicant:")
+# def selectdata():
+#     highlight = text_input.value
+#     selected = df
+   
+#     selected = selected[selected.first_name.str.contains(highlight)==True]
+    
+#     return selected
+
+
+l = layout([text_input],[data_table])
+tab2 = Panel(child = l, title= "Table")
+
+
 taptool = p.select(type = TapTool)
 url = "http://www.colors.commutercreative.com/@color/"
 taptool.callback = OpenURL(url=url)
 
 
-
-
-# def search(strin):
-#     text_value = text_input.value
-
-#     if df['first_name'].match(text_value) == True:
-#         return df.loc[df['first_name'] == text_value]
-#     else:
-#         return False
-    
-
 tabs = Tabs(tabs=[ tab1, tab2 ])
-# show the results
-# search(text_input)
+
+# selectdata()
+
 show (tabs)
 
 
