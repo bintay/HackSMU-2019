31664df4fcd4067063cdf8800a21871f66717f0a
Ben Taylor <bentaylor@bentaylor.me>
2019-02-02 18:16:01 -0600

    Revert "Added algorithm challengs"
    This reverts commit 73685780087e38fead74f8d17ecf4b2e46d10253.

--- applicant/index.js
+++ applicant/index.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = process.argv[2] || 3000;
+const port = 3000;
 
 app.use('/public', express.static('public'));
 
 app.get('/', function (req, res) {
-   res.sendFile(path.join(__dirname, 'frontend/index.html'));
+   res.sendFile(path.join(__dirname + '/frontend/index.html'));
 });
 
 app.listen(port, function () {
